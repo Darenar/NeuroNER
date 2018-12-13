a7796040c7ffd7d1216f470a379d163e75176a85
Andrey Tretyakov <a.tretyakov@office.ngs.ru>
2018-12-13 16:02:01 +0700

    add requires

--- setup.py
+++ setup.py
@@ -7,4 +7,21 @@ setup(
 
     packages=find_packages(),
 
+    package_data={
+        'neuroner': [
+            'parameters.ini',
+        ]
+    },
+
+    install_requires=[
+        'tensorflow==1.6.0rc1',
+        'networkx==2.1',
+        'matplotlib==2.2.0rc1',
+        'scikit-learn==0.19.1',
+        'scipy==1.0.0',
+        'pycorenlp==0.3.0',
+        'spacy==2.0.9',
+    ],
+
+
 )
